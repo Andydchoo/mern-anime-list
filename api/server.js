d65f74550f95a204bc0e9065230ba911a349ba1c
Andy Choo <andydchoo@gmail.com>
2023-02-16 07:55:35 -0600

    adding dotenv

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import listRouter from './routes/list-routes';
 import router from './routes/user-routes';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,9 +12,11 @@ app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/list", listRouter);
 
+const URI = process.env.MONGO_URI;
+
 mongoose
   .connect(
-    'mongodb+srv://admin:admin@cluster0.dnlpx58.mongodb.net/mern-anime-list?retryWrites=true&w=majority'
+    URI
   )
   .then(() => app.listen(5000))
   .then(() => console.log("Connected to DB. Listening to localhost 5000"))
